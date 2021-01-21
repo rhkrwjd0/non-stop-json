db707733b4318af8d19c5e1f504d88458ef52747
HeroNoah <58081455+HeroNoah@users.noreply.github.com>
2021-01-21 16:05:25 +0900

    Revert "Revert "Rhkrwjd0/issue2""

--- routes/components/db.js
+++ routes/components/db.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'logerdb.cafe24.com',
+    host: 'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     post: 3306,
-    user: 'logerdb',
-    password: 'Love20@!',
-    database: 'logerdb',
+    user: 'ekwqnqbogpjzx6tm',
+    password: 'vvkpxgqn64rfwsgy',
+    database: 'i08ik507tp6zc9lp',
     typeCast: function (field, next) {
         if (field.type == "VAR_STRING") {
             return field.string();
