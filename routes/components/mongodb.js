65355b71dd319cecfd83aecf1b579ebabf9e1fe6
rhkrwjd0 <62693319+rhkrwjd0@users.noreply.github.com>
2021-01-25 11:42:20 +0900

    [QUAC-31] API 목록 리스트업된 JSON으로 가짜API 서버배포 Fixes #9 mongo local -heroku test

--- routes/components/mongodb.js
+++ routes/components/mongodb.js
@@ -1,5 +1,5 @@
 
 //mongoDB 
-var url = 'mongodb://root:Love20%40%21@172.30.1.36:27017/'
-
+//var url = 'mongodb://root:Love20%40%21@172.30.1.36:27017/'  //heroku 
+var url = 'mongodb://localhost:27017/'  //local db
 exports.url = url;
