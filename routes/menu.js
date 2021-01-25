e799ac70e6f07472e853be1b7ea1fd967913495a
rhkrwjd0 <62693319+rhkrwjd0@users.noreply.github.com>
2021-01-25 15:49:20 +0900

    가짜json만들기

--- routes/menu.js
+++ routes/menu.js
@@ -6,39 +6,80 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var first = require("./function/first");
-var menu = require("../Quaca_MenuData.json");
+var Jmenu = require("../json/Menu.json");
+var JStoreInfo = require("../json/StoreInfo.json");
+var JUser = require("../json/User.json");
+var JUserPay = require("../json/UserPay.json");
+var JUserPayDetail = require("../json//UserPayDetail.json");
 const { json } = require('express');
 
 //quoka
 router.get('/', function (req, res, next) {
-    var amenu = req.query.menu;
-    var store = req.query.store;
-    console.log(amenu ,store);
-    if(amenu == "D"){
-        if(store == "1"){
-            res.send(menu.Menu.Store_1.Drink)
-        }else if(store =="2"){
-            res.send(menu.Menu.Store_2.Drink)
-        }else if(store =="3"){
-            res.send(menu.Menu.Store_3.Drink)
-        }
-    }else if(amenu == "B"){
-        if(store == "1"){
-            res.send(menu.Menu.Store_1.Bakery)
-        }else if(store =="2"){
-            res.send(menu.Menu.Store_2.Bakery)
-        }else if(store =="3"){
-            res.send(menu.Menu.Store_3.Bakery)
-        }
-    }else if(amenu == "G"){
-        if(store == "1"){
-            res.send(menu.Menu.Store_1.Goods)
-        }else if(store =="2"){
-            res.send(menu.Menu.Store_2.Goods)
-        }else if(store =="3"){
-            res.send(menu.Menu.Store_3.Goods)
-        }
-    }
+    console.log("history");
+     let email = req.query.email;
+     console.log(email);
+     res.send(JUserPay.UserPay['210121-'+email]);
+});
+router.get('/detail', function (req, res, next) {
+    console.log("detail");
+    let email = req.query.email;
+    console.log(email);
+    res.send(JUserPayDetail.UserPayDetail['210121-'+email]);
+});
+router.get('/user', function (req, res, next) {
+    console.log("user");
+    res.send(JUser);
+});
+router.get('/AllMenu', function (req, res, next) {
+    console.log("AllMenu");
+    res.send(Jmenu);
+});
+router.get('/StoreInfo', function (req, res, next) {
+    console.log("StoreInfo");
+    res.send(JStoreInfo);
 });
 
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(amenu == "D"){
+//     if(store == "1"){
+//         res.send(menu.Menu.Store_1.Drink)
+//     }else if(store =="2"){
+//         res.send(menu.Menu.Store_2.Drink)
+//     }else if(store =="3"){
+//         res.send(menu.Menu.Store_3.Drink)
+//     }
+// }else if(amenu == "B"){
+//     if(store == "1"){
+//         res.send(menu.Menu.Store_1.Bakery)
+//     }else if(store =="2"){
+//         res.send(menu.Menu.Store_2.Bakery)
+//     }else if(store =="3"){
+//         res.send(menu.Menu.Store_3.Bakery)
+//     }
+// }else if(amenu == "G"){
+//     if(store == "1"){
+//         res.send(menu.Menu.Store_1.Goods)
+//     }else if(store =="2"){
+//         res.send(menu.Menu.Store_2.Goods)
+//     }else if(store =="3"){
+//         res.send(menu.Menu.Store_3.Goods)
+//     }
+// }
